9171b140023e7a4f56968d0c9e9b752a6c0ac0a4
Alexander Bolgov <bolgov1997@mail.ru>
2021-05-20 17:19:10 +0500

    Выделение API в сервис

--- src/main/java/com/moneyconverter/MoneyConverter/service/CurrencyAPI.java
+++ src/main/java/com/moneyconverter/MoneyConverter/service/CurrencyAPI.java
@@ -6,14 +6,16 @@ import org.springframework.web.client.RestTemplate;
 
 public class CurrencyAPI {
     private static RestTemplate restTemplate = new RestTemplate();
+    private static final String key = "fb2e19bf90432dd02a0efbe5";
 
     public static double getCurrency(String code){
         double currencyValue = 0.0;
-        String response = restTemplate.getForObject("https://api.ratesapi.io/api/latest?base=" + code + "&symbols=RUB", String.class);
+        String response = restTemplate.getForObject("https://v6.exchangerate-api.com/v6/"+key+"/latest/"+code, String.class);
         JSONObject currencyJsonObj;
         try {
             currencyJsonObj = new JSONObject(response);
-            int i = (int) Math.round(Double.parseDouble(currencyJsonObj.getJSONObject("rates").getString("RUB")) * 100);
+            //System.out.println(currencyJsonObj.getJSONObject("conversion_rates").getString("RUB"));
+            int i = (int) Math.round(Double.parseDouble(currencyJsonObj.getJSONObject("conversion_rates").getString("RUB")) * 100);
             currencyValue = (double) i / 100.0;
 
         } catch (JSONException e) {
